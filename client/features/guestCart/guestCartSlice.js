340838b3be63e0beab2bc573efcfb4eb2380ccd3
Patrick Rooney <patrickrooney09@gmail.com>
2023-03-22 10:37:05 -0400

    pressing add to cart multiple times now increases quantity of book

--- client/features/guestCart/guestCartSlice.js
+++ client/features/guestCart/guestCartSlice.js
@@ -13,13 +13,37 @@ export const guestCartSlice = createSlice({
   reducers: {
     addBook: (state, action) => {
       console.log("book:", action);
-      console.log("state", state);
       // making copy of object so i can add a quantity property to it
       let book = Object.assign({}, action.payload);
-      book.quantity = 1;
-      state.push(book);
-      sessionStorage.setItem(book.title, JSON.stringify(book));
-      console.log(sessionStorage);
+
+      if (state.length > 0) {
+        state.filter((currentBook, index) => {
+          if (book.title === currentBook.title) {
+            currentBook.quantity++;
+            sessionStorage.removeItem(book.title);
+            sessionStorage.setItem(
+              currentBook.title,
+              JSON.stringify(currentBook)
+            );
+          } else if (
+            index === state.length &&
+            book.title !== currentBook.title
+          ) {
+            book.quantity = 1;
+            state.push(book);
+            sessionStorage.setItem(book.title, JSON.stringify(book));
+          }
+        });
+      } else {
+        book.quantity = 1;
+        state.push(book);
+        sessionStorage.setItem(book.title, JSON.stringify(book));
+      }
+
+      // book.quantity = 1;
+      // state.push(book);
+      // sessionStorage.setItem(book.title, JSON.stringify(book));
+      // console.log(sessionStorage);
     },
     removeBook: (state, action, index) => {
       state.splice(index, 1);
